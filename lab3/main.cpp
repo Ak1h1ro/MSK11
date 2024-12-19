c93204d2721d864c45d7f99ce163c95bb0f4ae56
Ak1h1ro <pavalex002@gmail.com>
2024-12-19 12:09:43 +0300

    lab3 one thread ver

---
+++ lab3/main.cpp
@@ -0,0 +1,106 @@
+#include "vector.h"
+#include "functions.h"
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <omp.h>
+
+
+void ExecutionOneThread(Vector<int>& vec1, Vector<int>& vec2) {
+    auto start = std::chrono::high_resolution_clock::now(); // Начало замера времени
+
+    auto [minMaxPair1, minMaxPair2] = findMinMax(vec1);
+    auto [minValue, minIndex] = minMaxPair1;
+    auto [maxValue, maxIndex] = minMaxPair2;
+
+    double avg = calculateAverage(vec1);
+    double sum = SumOfElements(vec1);
+    double manhattanNorm = calculateManhattanNorm(vec1);
+    double euclideanNorm = calculateEuclideanNorm(vec1);
+
+    double dotProd = dotProduct(vec1, vec2);
+
+    // Вывод результатов
+    std::cout << "Min: " << minValue << " at index " << minIndex << "\n";
+    std::cout << "Max: " << maxValue << " at index " << maxIndex << "\n";
+    std::cout << "Average: " << avg << "\n";
+    std::cout << "Sum: " << sum << "\n";
+    std::cout << "Manhattan Norm: " << manhattanNorm << "\n";
+    std::cout << "Euclidean Norm: " << euclideanNorm << "\n";
+    std::cout << "Dot Product: " << dotProd << "\n";
+    auto end = std::chrono::high_resolution_clock::now(); // Конец замера времени
+    std::chrono::duration<double> duration = end - start; // Вычисляем продолжительность
+    std::cout << "Execution time (One Thread): " << duration.count() << " seconds\n";
+}
+
+
+
+int main() {
+  //  Vector<int> vec(10); // Создаем вектор целых чисел с размером 10
+    Vector<int> vec1(100000000); // Создаем вектор целых чисел с размером 10
+    Vector<int> vec2(100000000); // Создаем вектор целых чисел с размером 10
+
+    try {
+        // тут веткор с константными данными
+        /*
+        //Константные значения вектора vec
+        vec.initializeWithConstant(8); // Инициализируем все элементы значением 8
+
+        std::cout << "Size of vector: " << vec.getSize() << std::endl;
+        for (size_t i = 0; i < vec.getSize(); ++i) {
+            std::cout << "Element " << i << ": " << vec[i] << std::endl;
+        }*/
+
+        //Рандомные значения вектора vec1
+         vec1.initializeWithRandomNumbers(-10, 10); // Инициализируем случайными числами от -10 до 10
+        std::cout << "size of vector1: " << vec1.getSize() << std::endl;
+        std::cout << "After inicialization random numbers vector1:" << std::endl;
+        /*
+        for (size_t i = 0; i < vec1.getSize(); ++i) {
+            std::cout << "Element " << i << ": " << vec1[i] << std::endl;
+        }*/
+
+        //Рандомные значения вектора vec2
+         vec2.initializeWithRandomNumbers(-10, 11); // Инициализируем случайными числами от -10 до 10
+        std::cout << "size of vector2: " << vec2.getSize() << std::endl;
+        std::cout << "After inicialization random numbers vector2:" << std::endl;
+      /*  for (size_t i = 0; i < vec2.getSize(); ++i) {
+            std::cout << "Element " << i << ": " << vec2[i] << std::endl;
+        }
+
+        */
+
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+
+
+    std::cout<< "--------------------OneThread-------------------------------------"<<"\n" << std::endl;
+
+        ExecutionOneThread(vec1,vec2);
+
+
+
+
+        /*
+
+        // Экспорт данных в файл.
+        vec1.exportToFile("output.txt");
+
+        //Импорт данных из файла
+        // Создаем новый объект для импорта.
+        Vector<int> importedVec; // Создаем новый вектор
+        importedVec.initializeWithConstant(0); // Инициализируем его перед импортом
+
+        importedVec.importFromFile("output.txt");
+
+        // Выводим импортированные данные.
+        std::cout << "Imported data:" << std::endl;
+        for (size_t i = 0; i < importedVec.getSize(); ++i) {
+            std::cout << "Element " << i << ": " << importedVec[i] << std::endl;
+        }
+
+        */
+    return 0;
+}
